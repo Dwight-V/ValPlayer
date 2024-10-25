2a38b26f7d24a6d1f817817fab944b9754e0a965
Dwight-V <dvight@duck.com>
2024-10-25 16:51:04 -0400

    Initial directory RecycleView

---
+++ app/src/main/java/com/example/ValPlayer/AudioFile.java
@@ -0,0 +1,54 @@
+package com.example.ValPlayer;
+
+import android.provider.MediaStore;
+
+// Is used in the recycleviewer for listing all audio files.
+// This class represents an individual item that the RecycleViewer can hold.
+// From https://github.com/codepath/android_guides/wiki/Using-the-RecyclerView
+public class AudioFile {
+    private long id;
+    private String song;
+    private String artist;
+    private String album;
+    private long duration;
+    private String path;
+
+    AudioFile (long id, String song, String artist, String album, long duration, String path) {
+        this.id = id;
+        this.song = song;
+        this.artist = artist;
+        this.album = album;
+        this.duration = duration;
+        this.path = path;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public String getFormattedDuration() {
+        long durInSec = duration / 1000;
+        return String.format("%d:%d:%02d", durInSec / 3600, durInSec % 3600 / 60, durInSec % 60);
+    }
+}
--- app/src/main/java/com/example/ValPlayer/MainActivity.java
+++ app/src/main/java/com/example/ValPlayer/MainActivity.java
@@ -4,22 +4,35 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final int STORAGE_PERMISSION_CODE = 23;
+
+    private RecyclerView rvDirectory;
 
     private ActivityResultLauncher<Intent> storageActivityResultLauncher =
             registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
@@ -43,32 +56,84 @@ public class MainActivity extends AppCompatActivity {
                     });
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == STORAGE_PERMISSION_CODE){
-            if(grantResults.length > 0){
-                boolean write = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                boolean read = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-
-                if(read && write){
-                    Toast.makeText(MainActivity.this, "Storage Permissions Granted", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this, "Storage Permissions Denied", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rvDirectory = findViewById(R.id.recycleview_directory);
+
         if (!checkStoragePermissions()) {
             requestForStoragePermissions();
         }
+
+        addAudioFilesToRecycle();
+
     }
 
+    // region accessing files
+    public void addAudioFilesToRecycle() {
+        ArrayList<AudioFile> arrListAudio = new ArrayList<>();
+
+        Uri audioUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        ContentResolver contentResolver = this.getContentResolver();
+
+        // These are the columns of metadata you want.
+        String[] projection = new String[] {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA // Path to file
+        };
+
+//        String selection = MediaStore.Audio.Media.DATA + "== /storage/emulated/0/Music";
+
+//        String[] selectionArgs = new String[] {
+//                values-of-placeholder-variables
+//        };
+//        String sortOrder = sql-order-by-clause;
+
+        // Execute the query
+        try (Cursor cursor = contentResolver.query(
+                audioUri,
+                projection,
+                null,
+                null,
+                null)) {
+
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    // Retrieve data from the cursor
+                    long id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID));
+                    String title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
+                    String artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST));
+                    String album = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM));
+                    long duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION));
+                    String path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+
+//                    long durInSec = duration / 1000;
+//                    String strDur = String.format("%d:%d:%02d", durInSec / 3600, durInSec % 3600 / 60, durInSec % 60);
+
+//                    Log.d("AudioFile", "ID: " + id + " Title: " + title + " Artist: " + artist +
+//                            " Album: " + album + " Duration: " + strDur + " Path: " + path);
+
+                    arrListAudio.add(new AudioFile(id, title, artist, album, duration, path));
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        AudioFileAdapter adapter = new AudioFileAdapter(arrListAudio);
+        rvDirectory.setAdapter(adapter);
+        rvDirectory.setLayoutManager(new LinearLayoutManager(this));
+    }
+    // endregion
+
+
+    // region Requesting file permission
     public boolean checkStoragePermissions(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
             //Android is 11 (R) or above
@@ -107,6 +172,24 @@ public class MainActivity extends AppCompatActivity {
                     STORAGE_PERMISSION_CODE
             );
         }
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.length > 0){
+                boolean write = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean read = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if(read && write){
+                    Toast.makeText(MainActivity.this, "Storage Permissions Granted", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Storage Permissions Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    // endregion
+
 }
